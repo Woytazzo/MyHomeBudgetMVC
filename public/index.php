063d24f5d2f0c48f55490d7aef772094ab53ad90
Woytazzo <31483046+Woytazzo@users.noreply.github.com>
2020-11-20 11:43:11 +0100

    Add files via upload

---
+++ public/index.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Front controller
+ *
+ * PHP version 7.0
+ */
+
+/**
+ * Composer
+ */
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+
+/**
+ * Error and Exception handling
+ */
+error_reporting(E_ALL);
+set_error_handler('Core\Error::errorHandler');
+set_exception_handler('Core\Error::exceptionHandler');
+
+
+/**
+ * Sessions
+ */
+session_start();
+
+
+/**
+ * Routing
+ */
+$router = new Core\Router();
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('login', ['controller' => 'Login', 'action' => 'new']);
+$router->add('income', ['controller' => 'Income', 'action' => 'new']);
+$router->add('expense', ['controller' => 'expense', 'action' => 'new']);
+$router->add('balance', ['controller' => 'balance', 'action' => 'check']);
+$router->add('logout', ['controller' => 'Login', 'action' => 'destroy']);
+$router->add('password/reset/{token:[\da-f]+}', ['controller' => 'Password', 'action' => 'reset']);
+$router->add('{controller}/{action}');
+
+$router->dispatch($_SERVER['QUERY_STRING']);
